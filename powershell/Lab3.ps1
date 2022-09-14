c70f6aa1c238c5325406b24c33e43078abcb9d2d
scott2r3 <111697637+scott2r3@users.noreply.github.com>
2022-09-14 00:55:23 -0400

    Add files via upload

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,14 @@
+﻿function getIP{(Get-NetIPAddress).IPv4Address | Select-String "192*"}
+$IP = getIP
+function userGet{get-localuser | select-string "admin"}
+$USERCURRENT = userGet
+$HOSTNAME = hostname
+$VERSION = $HOST.Version.Major
+$DATE = Get-Date
+
+function bodyy{
+"This machines IP is $IP. User is $USERCURRENT. Hostname is $HOSTNAME. Powershell Version $VERSION. Today's Date is $DATE"
+}
+$BODY = bodyy
+
+Send-MailMessage -To "reedws@ucmail.uc.edu" -From "scotttester2307@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,5 @@
+﻿$IP = getIP
+
+Write-Host("This machines IP is: $IP")
+
+Write-Host("This machines IP is: {0}" -f $IP)
