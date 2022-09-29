9f1aa7faf6b470a392997a1eb1f7a090c43bd8d6
scott2r3 <111697637+scott2r3@users.noreply.github.com>
2022-09-29 18:57:28 -0400

    Create Lab5.py

---
+++ Labs/Lab5.py
@@ -0,0 +1,11 @@
+import datetime
+while True:
+    try:
+        userb = input("Enter your birthday in September 17 2002 form: ")
+        birth = datetime.datetime.strptime(userb, "%B %d %Y")
+        break
+    except:
+        print("Format is incorrect please try in September 17 2002 format!")
+dayof = datetime.datetime.today()
+answer = (dayof - birth).total_seconds()
+print("You are ",answer, " seconds old")
