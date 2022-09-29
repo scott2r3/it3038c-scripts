e48dcaed2916084ca0c68a77556b3a86707724a9
scott2r3 <111697637+scott2r3@users.noreply.github.com>
2022-09-29 19:00:01 -0400

    Birthday Seconds
    Birthday in seconds Python script.

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -9,3 +9,4 @@ while True:
 dayof = datetime.datetime.today()
 answer = (dayof - birth).total_seconds()
 print("You are ",answer, " seconds old")
+# Enter your birthday as I have it formatted for script to work.
