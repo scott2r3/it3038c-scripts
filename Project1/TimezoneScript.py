d08189a1eebccc6ba445e046506070489ec13adb
scott2r3 <111697637+scott2r3@users.noreply.github.com>
2022-10-04 23:03:41 -0400

    TimezoneScript
    The script will ask the user for a timezone they wish to know the time in and then run through a list of those times and present that timezones time info. I used a website to get the information I needed to understand what each command does in library. The site was geeksforgeeks "get current time in different timezone using python". I only used top section about importing and calling specific information in the print statement.

---
+++ Project1/TimezoneScript.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import pytz
+#This imports the time stuff we will need to get times for each timezone.
+userINList = input("Hello to the time script. If you know your timezone in Pytz form press N and hit enter. If not type 'list'. ")
+rlist = 'list'
+if userINList==rlist:
+    for i in pytz.all_timezones:
+        print(i)
+#Everything above is about asking the user if they need to see what timezone they are and how it needs to be inputed into the input
+userIN = input("Hello, what timezone are you from? Please enter in Pytz timezone form in the list above.")
+rlist = 'list'
+UIT = datetime.now(pytz.timezone(userIN))
+#This is us asking the user what timezone and then storing it
+print("The time in", (userIN), "is:", UIT.strftime("%Y:%m:%d %H:%M:%S %Z %z"))
+#Here we output the time in the timezone the user stated
